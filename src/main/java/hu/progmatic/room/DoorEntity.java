466de8ccb9173136adcf5aa32e97eba451ead4ea
lia205 <lkatona@protonmail.com>
2022-01-31 14:21:15 +0100

    mao

---
+++ src/main/java/hu/progmatic/room/DoorEntity.java
@@ -0,0 +1,19 @@
+package hu.progmatic.room;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DoorEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Integer id;
+    @OneToMany
+    RoomEntity room;
+}
