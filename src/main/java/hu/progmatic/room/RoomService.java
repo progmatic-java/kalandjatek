9d3571323f7e2cfe8d226eb481379e7a0b0d9331
Boróka <ozsvar.boroka@gmail.com>
2022-01-31 14:53:58 +0100

    mao

--- src/main/java/hu/progmatic/room/RoomService.java
+++ src/main/java/hu/progmatic/room/RoomService.java
@@ -1,6 +1,7 @@
 package hu.progmatic.room;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,7 +16,25 @@ public class RoomService implements InitializingBean {
             RoomEntity.builder().name("Erdő").door(new DoorEntity()).build()
     );
 
+    @Autowired
+    private RoomRepository roomRepository;
+
+    public List<RoomEntity> findAllRooms() {
+        return roomRepository.findAll().stream().toList();
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
+        if (roomRepository.count() == 0) {
+            roomRepository.saveAll(
+                    rooms.stream()
+                            .map(dto -> RoomEntity.builder()
+                                    .name(dto.getName())
+                                    .door(dto.getDoor())
+                                    .build())
+                            .toList()
+            );
+        }
     }
+
 }
