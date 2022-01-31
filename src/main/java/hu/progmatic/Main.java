b3066da80d6a9a15ff59d5e6ec3bbf9aeb625a13
Krisz <krisztian.kovats94@gmail.com>
2022-01-31 11:46:55 +0100

    khélypek

--- src/main/java/hu/progmatic/Main.java
+++ src/main/java/hu/progmatic/Main.java
@@ -1,8 +1,4 @@
 package hu.progmatic;
 
 public class Main {
-  public static void main(String[] args) {
-    HelloWorld hello = new HelloWorld();
-    System.out.println(hello.getHelloWorld());
-  }
 }
