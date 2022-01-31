46e2d1b372f482867c30de3da4f6346403696a70
lia205 <lkatona@protonmail.com>
2022-01-31 14:32:02 +0100

    mao

--- src/main/java/hu/progmatic/room/DoorEntity.java
+++ src/main/java/hu/progmatic/room/DoorEntity.java
@@ -3,6 +3,8 @@ package hu.progmatic.room;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,8 +14,9 @@ import javax.persistence.*;
 @Builder
 public class DoorEntity {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     @OneToMany(cascade = CascadeType.ALL)
-    RoomEntity room;
+    @Builder.Default
+    List<RoomEntity> room = new ArrayList<>();
 }
