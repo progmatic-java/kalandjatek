630f5038734337d64bb4626edc6d2ff02b29ce93
Boróka <ozsvar.boroka@gmail.com>
2022-01-31 14:11:48 +0100

    mao

---
+++ src/main/java/hu/progmatic/room/RoomEntity.java
@@ -0,0 +1,15 @@
+package hu.progmatic.room;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoomEntity {
+
+}
