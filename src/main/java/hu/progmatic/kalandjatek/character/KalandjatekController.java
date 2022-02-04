392d0faf591091c0582143f83378e79329b412cd
Krisz <krisztian.kovats94@gmail.com>
2022-02-04 13:29:05 +0100

    munka

--- src/main/java/hu/progmatic/kalandjatek/character/KalandjatekController.java
+++ src/main/java/hu/progmatic/kalandjatek/character/KalandjatekController.java
@@ -71,6 +71,21 @@ public class KalandjatekController {
         return "/kalandjatek/karaktertemplate";
     }
 
+    @PostMapping("/kalandjatek/unwantedcharacter/{id}")
+    public String unwantedCharacter(
+            @ModelAttribute("testAnswer")
+            @PathVariable Integer id,
+            Model model
+    ) {
+        CharacterEntity character = characterService.getById(id);
+        Answer resultCharacter = character.getAnswer();
+        characterService.delete(id);
+        model.addAttribute("chosenCharacter", resultCharacter);
+        return "/kalandjatek/szemelyisegtesztmixelt";
+    }
+
+
+
     @GetMapping("/kalandjatek/introduction/{id}")
     public String intro(@PathVariable Integer id, Model model) {
         CharacterEntity chosenCharacter = characterService.getById(id);
