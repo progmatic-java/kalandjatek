61b243b4974701dcb1b5d46bf393f05806a57d64
Krisz <krisztian.kovats94@gmail.com>
2022-02-04 11:20:29 +0100

    munka

--- src/main/java/hu/progmatic/kalandjatek/room/RoomController.java
+++ src/main/java/hu/progmatic/kalandjatek/room/RoomController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RoomController {
@@ -22,6 +23,8 @@ public class RoomController {
     public String roomPage() {
         return "/kalandjatek/room";
     }
+
+
 
     @GetMapping("/kalandjatek/characterpage/{characterId}/room/{roomId}")
     public String gameStarter(
