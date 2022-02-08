1d058ab216ff8ee3b6df333cf36771e2c8f0f7b8
Krisz <krisztian.kovats94@gmail.com>
2022-02-08 12:24:16 +0100

    munka

--- src/main/java/hu/progmatic/kalandjatek/character/CharacterController.java
+++ src/main/java/hu/progmatic/kalandjatek/character/CharacterController.java
@@ -14,11 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.List;
 
 
 @Controller
-public class AdventureGameController {
+public class CharacterController {
 
 
 
