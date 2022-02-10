5009848fd08c085808571f4ecdfda614419cd868
PamukBence <pamukbence@gmail.com>
2022-02-10 13:10:29 +0100

    refaktorálás

--- src/main/java/hu/progmatic/adventuregame/init/ForestLakeInit.java
+++ src/main/java/hu/progmatic/adventuregame/init/ForestLakeInit.java
@@ -6,45 +6,45 @@ import hu.progmatic.adventuregame.npc.NPC;
 
 import java.util.List;
 
-public class ForestLakeInit extends InitRoom{
-    @Override
-    public String getName() {
-        return "The Forest Lake\n";
-    }
+public class ForestLakeInit extends InitRoom {
+  @Override
+  public String getName() {
+    return "The Forest Lake\n";
+  }
 
-    @Override
-    List<NPC> getInitNpcs() {
-        return List.of(
-                NPC.builder()
-                        .name("Fairy of the Lake")
-                        .description("This beautiful fairy loves to seduce young travelers!")
-                        .build()
-        );
-    }
+  @Override
+  List<NPC> getInitNpcs() {
+    return List.of(
+        NPC.builder()
+            .name("Fairy of the Lake")
+            .description("This beautiful fairy loves to seduce young travelers!")
+            .build()
+    );
+  }
 
-    @Override
-    List<Item> getInitItems() {
-        return List.of(
-                Item.builder()
-                        .itemName("Fairy Shield")
-                        .description("A magical shield that is useful against dark powers!")
-                        .typeOfItem(ItemEnum.SHIELD)
-                        .build(),
-                Item.builder()
-                        .itemName("Fairy Panties")
-                        .description("Hmmm... What's that doing floating in the water?")
-                        .typeOfItem(ItemEnum.SHIELD)
-                        .build(),
-                Item.builder()
-                        .itemName("Rotten fish")
-                        .description("They exude a disgusting stench!")
-                        .typeOfItem(ItemEnum.JUNK)
-                        .build()
-        );
-    }
+  @Override
+  List<Item> getInitItems() {
+    return List.of(
+        Item.builder()
+            .itemName("Fairy Shield")
+            .description("A magical shield that is useful against dark powers!")
+            .typeOfItem(ItemEnum.SHIELD)
+            .build(),
+        Item.builder()
+            .itemName("Fairy Panties")
+            .description("Hmmm... What's that doing floating in the water?")
+            .typeOfItem(ItemEnum.SHIELD)
+            .build(),
+        Item.builder()
+            .itemName("Rotten fish")
+            .description("They exude a disgusting stench!")
+            .typeOfItem(ItemEnum.JUNK)
+            .build()
+    );
+  }
 
-    @Override
-    public String getRoomImgRef() {
-        return "https://i.pinimg.com/originals/da/52/4e/da524e6c19b3a3933daa7793d9b34ad4.jpg";
-    }
+  @Override
+  public String getRoomImgRef() {
+    return "https://i.pinimg.com/originals/da/52/4e/da524e6c19b3a3933daa7793d9b34ad4.jpg";
+  }
 }
--- src/main/java/hu/progmatic/adventuregame/init/ThePathToTheAcademyInit.java
+++ src/main/java/hu/progmatic/adventuregame/init/ThePathToTheAcademyInit.java
@@ -8,40 +8,40 @@ import java.util.List;
 
 public class ThePathToTheAcademyInit extends InitRoom {
 
-    @Override
-    public String getName() {
-        return "The Path to the Academy";
-    }
+  @Override
+  public String getName() {
+    return "The Path to the Academy";
+  }
 
-    @Override
-    List<NPC> getInitNpcs() {
-        return List.of();
-    }
+  @Override
+  List<NPC> getInitNpcs() {
+    return List.of();
+  }
 
-    @Override
-    List<Item> getInitItems() {
-        return List.of(
-                Item.builder()
-                        .itemName("Ripped books")
-                        .description("Someone got expelled...")
-                        .typeOfItem(ItemEnum.JUNK)
-                        .build(),
-                Item.builder()
-                        .itemName("Empty potion bottles")
-                        .description("These younglings don't care about the climate??")
-                        .typeOfItem(ItemEnum.JUNK)
-                        .build()
-        );
-    }
+  @Override
+  List<Item> getInitItems() {
+    return List.of(
+        Item.builder()
+            .itemName("Ripped books")
+            .description("Someone got expelled...")
+            .typeOfItem(ItemEnum.JUNK)
+            .build(),
+        Item.builder()
+            .itemName("Empty potion bottles")
+            .description("These younglings don't care about the climate??")
+            .typeOfItem(ItemEnum.JUNK)
+            .build()
+    );
+  }
 
-    @Override
-    public String getRoomImgRef() {
-        return "https://i.imgur.com/E7k4guh.jpg";
-    }
+  @Override
+  public String getRoomImgRef() {
+    return "https://i.imgur.com/E7k4guh.jpg";
+  }
 
-    @Override
-    public String getRoomAudio() {
-        return "music/Damp.mp3";
-    }
+  @Override
+  public String getRoomAudio() {
+    return "music/Damp.mp3";
+  }
 }
 
