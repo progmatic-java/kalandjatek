4dbf014fc7cbb06e6ec8ec74c10ffdf05cc29739
Krisz <krisztian.kovats94@gmail.com>
2022-02-19 15:16:16 +0100

    munka

--- src/main/java/hu/progmatic/adventuregame/character/CharacterEntity.java
+++ src/main/java/hu/progmatic/adventuregame/character/CharacterEntity.java
@@ -12,27 +12,28 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Builder
 public class CharacterEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
-  private String name;
-  @Enumerated(EnumType.STRING)
-  Race race;
-  private String description;
-  private Integer hp;
-  private Integer mp;
-  private Integer gold;
-  private Integer attack;
-  private Integer defence;
-  private String imgRef;
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn
-  Answer answer;
-  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn
-  Inventory inventory;
-  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn
-  Inventory activeInventory;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    Race race;
+    private String description;
+    private Integer hp;
+    private Integer mp;
+    private Integer gold;
+    private Integer attack;
+    private Integer defence;
+    private String imgRef;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn
+    Answer answer;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn
+    Inventory inventory;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn
+    @Builder.Default
+    Inventory activeInventory = new Inventory();
 
 }
