9da62cec3c2e1d846f6b6e800a1384b7f8eb9f99
PamukBence <pamukbence@gmail.com>
2022-02-21 13:38:33 +0100

    Changes by GombaMágus

--- src/main/java/hu/progmatic/adventuregame/character/CharacterService.java
+++ src/main/java/hu/progmatic/adventuregame/character/CharacterService.java
@@ -16,19 +16,19 @@ import java.util.Map;
 @Service
 public class CharacterService implements InitializingBean {
 
-    @Autowired
-    private CharacterRepository characterRepository;
-    @Autowired
-    private InventoryService inventoryService;
+  @Autowired
+  private CharacterRepository characterRepository;
+  @Autowired
+  private InventoryService inventoryService;
 
 
-    public CharacterEntity save(CharacterEntity character) {
-        return characterRepository.save(character);
-    }
+  public CharacterEntity save(CharacterEntity character) {
+    return characterRepository.save(character);
+  }
 
-    public CharacterEntity getById(Integer id) {
-        return characterRepository.getById(id);
-    }
+  public CharacterEntity getById(Integer id) {
+    return characterRepository.getById(id);
+  }
 
     public CharacterDto getCharacterDtoById(Integer id) {
         CharacterEntity entity = characterRepository.getById(id);
@@ -52,172 +52,175 @@ public class CharacterService implements InitializingBean {
     }
 
 
-    @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
-    public void delete(Integer id) {
-        characterRepository.deleteById(id);
+  @RolesAllowed(UserType.Roles.USER_WRITE_ROLE)
+  public void delete(Integer id) {
+    characterRepository.deleteById(id);
+  }
+
+  public List<CharacterEntity> saveAll(List<CharacterEntity> list) {
+    return characterRepository.saveAllAndFlush(list);
+  }
+
+  public List<CharacterEntity> findAllByName(String name) {
+    return characterRepository.findAllByNameContains(name);
+  }
+
+  public List<CharacterEntity> findAllByRace(Race race) {
+    return characterRepository.findAllByRace(race);
+  }
+
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    Inventory elf = Inventory.builder().build();
+    Inventory orc = Inventory.builder().build();
+    Inventory human = Inventory.builder().build();
+    Inventory reptilian = Inventory.builder().build();
+
+    Item shield = Item.builder().itemName("Shield").typeOfItem(ItemEnum.SHIELD).description("He protect.").inventory(elf).build();
+    Item shieldOrc = Item.builder().itemName("Shield").typeOfItem(ItemEnum.SHIELD).description("He protect.").inventory(orc).build();
+    Item sword = Item.builder().itemName("Sword").typeOfItem(ItemEnum.ATTACK).description("He attack.").inventory(human).build();
+    Item mace = Item.builder().itemName("Mace").typeOfItem(ItemEnum.ATTACK).description("So big.").inventory(orc).build();
+    Item rock = Item.builder().itemName("Rock").typeOfItem(ItemEnum.ATTACK).description("Tasty").inventory(reptilian).build();
+
+    elf.getItems().add(shield);
+    orc.getItems().add(mace);
+    orc.getItems().add(shieldOrc);
+    human.getItems().add(sword);
+    reptilian.getItems().add(rock);
+
+    if (findAll().isEmpty()) {
+      List<CharacterEntity> buildingCharacter = List.of(
+          CharacterEntity
+              .builder()
+              .name("Vallak")
+              .race(Race.ORC)
+              .hp(200)
+              .mp(50)
+              .gold(100)
+              .attack(15)
+              .defence(10)
+              .imgRef("https://i.imgur.com/lXcw2hg.png")
+              .description(Race.ORC.description)
+              .inventory(Inventory.builder().build())
+              .activeInventory(orc)
+              .build(),
+          CharacterEntity.builder().name("Lyah")
+              .race(Race.ELF)
+              .hp(100)
+              .mp(200)
+              .gold(200)
+              .attack(8)
+              .defence(20)
+              .imgRef("https://i.imgur.com/13NVorA.png")
+              .description(Race.ELF.description)
+              .inventory(elf)
+              .build(),
+          CharacterEntity
+              .builder()
+              .name("Bence")
+              .race(Race.HUMAN)
+              .hp(150)
+              .mp(100)
+              .gold(200)
+              .attack(10)
+              .defence(16)
+              .imgRef("https://i.imgur.com/MQRZudq.png")
+              .description(Race.HUMAN.description)
+              .inventory(human)
+              .build(),
+          CharacterEntity
+              .builder()
+              .name("Mark Zuckerberg")
+              .race(Race.REPTILIAN)
+              .hp(150)
+              .mp(150)
+              .gold(200)
+              .attack(12)
+              .defence(13)
+              .imgRef("https://i.imgur.com/0f45Fce.png")
+              .description(Race.REPTILIAN.description)
+              .inventory(reptilian)
+              .build());
+      List<CharacterEntity> characters = saveAll(buildingCharacter);
+
     }
+  }
 
-    public List<CharacterEntity> saveAll(List<CharacterEntity> list) {
-        return characterRepository.saveAllAndFlush(list);
+  public Race getResults(Answer answer) {
+    Map<Race, Integer> answerEvaluation = new HashMap<>();
+    fillEvaluationMap(answerEvaluation, answer.getRace1());
+    fillEvaluationMap(answerEvaluation, answer.getRace2());
+    fillEvaluationMap(answerEvaluation, answer.getRace3());
+    fillEvaluationMap(answerEvaluation, answer.getRace4());
+    fillEvaluationMap(answerEvaluation, answer.getRace5());
+    fillEvaluationMap(answerEvaluation, answer.getRace6());
+    fillEvaluationMap(answerEvaluation, answer.getRace7());
+    fillEvaluationMap(answerEvaluation, answer.getRace8());
+    return getMaxKey(answerEvaluation);
+  }
+
+  private Race getMaxKey(Map<Race, Integer> answerEvaluation) {
+    int maxValue = 0;
+    Race maxKey = null;
+    for (Map.Entry<Race, Integer> entry : answerEvaluation.entrySet()) {
+      if (maxValue < entry.getValue()) {
+        maxValue = entry.getValue();
+        maxKey = entry.getKey();
+      }
     }
+    return maxKey;
+  }
 
-    public List<CharacterEntity> findAllByName(String name) {
-        return characterRepository.findAllByNameContains(name);
+  private void fillEvaluationMap(Map<Race, Integer> answerEvaluation, Race race) {
+    Integer value = answerEvaluation.getOrDefault(race, 0);
+    answerEvaluation.put(race, value + 1);
+  }
+
+  public List<CharacterEntity> findAll() {
+    return characterRepository.findAll();
+  }
+
+  public CharacterEntity getResultCharacter(Answer answer) {
+    Inventory charInv = Inventory.builder().build();
+    List<Item> startingItems = List.of(
+        Item.builder().itemName("Sword").typeOfItem(ItemEnum.ATTACK).description("He attack.").inventory(charInv).build(),
+        Item.builder().itemName("Shield").typeOfItem(ItemEnum.SHIELD).description("He protect.").inventory(charInv).build(),
+        Item.builder().itemName("Potion").typeOfItem(ItemEnum.CONSUMABLE).description("He heals.").inventory(charInv).build()
+    );
+    charInv.setItems(startingItems);
+    Race characterRace = getResults(answer);
+    return CharacterEntity.builder()
+        .name(answer.getName())
+        .hp(characterRace.hp)
+        .mp(characterRace.mp)
+        .gold(characterRace.gold)
+        .imgRef(characterRace.img)
+        .description(characterRace.description)
+        .inventory(charInv)
+        .attack(characterRace.attack)
+        .defence(characterRace.defence)
+        .race(characterRace)
+        .answer(answer)
+        .build();
+  }
+
+  public Integer getIdByName(String name) {
+    return characterRepository.getCharacterByName(name).orElseThrow().getId();
+  }
+
+  public boolean moveItemToPlayer(Integer characterId, Integer inventoryId, Integer itemId) {
+    Item item = inventoryService.getItemEntityById(itemId);
+    Inventory inventory = inventoryService.getInventoryEntityById(inventoryId);
+    CharacterEntity character = characterRepository.getById(characterId);
+
+    if (item.getValue() > character.getGold()) {
+      return false;
     }
-
-    public List<CharacterEntity> findAllByRace(Race race) {
-        return characterRepository.findAllByRace(race);
-    }
-
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        Inventory elf = Inventory.builder().build();
-        Inventory orc = Inventory.builder().build();
-        Inventory human = Inventory.builder().build();
-        Inventory reptilian = Inventory.builder().build();
-
-        Item shield = Item.builder().itemName("Shield").typeOfItem(ItemEnum.SHIELD).description("He protect.").inventory(elf).build();
-        Item sword = Item.builder().itemName("Sword").typeOfItem(ItemEnum.ATTACK).description("He attack.").inventory(human).build();
-        Item mace = Item.builder().itemName("Mace").typeOfItem(ItemEnum.ATTACK).description("So big.").inventory(orc).build();
-        Item rock = Item.builder().itemName("Rock").typeOfItem(ItemEnum.ATTACK).description("Tasty").inventory(reptilian).build();
-
-        elf.getItems().add(shield);
-        orc.getItems().add(mace);
-        human.getItems().add(sword);
-        reptilian.getItems().add(rock);
-
-        if (findAll().isEmpty()) {
-            List<CharacterEntity> buildingCharacter = List.of(
-                    CharacterEntity
-                            .builder()
-                            .name("Vallak")
-                            .race(Race.ORC)
-                            .hp(200)
-                            .mp(50)
-                            .gold(100)
-                            .attack(15)
-                            .defence(10)
-                            .imgRef("https://i.imgur.com/lXcw2hg.png")
-                            .description(Race.ORC.description)
-                            .inventory(orc)
-                            .build(),
-                    CharacterEntity.builder().name("Lyah")
-                            .race(Race.ELF)
-                            .hp(100)
-                            .mp(200)
-                            .gold(200)
-                            .attack(8)
-                            .defence(20)
-                            .imgRef("https://i.imgur.com/13NVorA.png")
-                            .description(Race.ELF.description)
-                            .inventory(elf)
-                            .build(),
-                    CharacterEntity
-                            .builder()
-                            .name("Bence")
-                            .race(Race.HUMAN)
-                            .hp(150)
-                            .mp(100)
-                            .gold(200)
-                            .attack(10)
-                            .defence(16)
-                            .imgRef("https://i.imgur.com/MQRZudq.png")
-                            .description(Race.HUMAN.description)
-                            .inventory(human)
-                            .build(),
-                    CharacterEntity
-                            .builder()
-                            .name("Mark Zuckerberg")
-                            .race(Race.REPTILIAN)
-                            .hp(150)
-                            .mp(150)
-                            .gold(200)
-                            .attack(12)
-                            .defence(13)
-                            .imgRef("https://i.imgur.com/0f45Fce.png")
-                            .description(Race.REPTILIAN.description)
-                            .inventory(reptilian)
-                            .build());
-            List<CharacterEntity> characters = saveAll(buildingCharacter);
-
-        }
-    }
-
-    public Race getResults(Answer answer) {
-        Map<Race, Integer> answerEvaluation = new HashMap<>();
-        fillEvaluationMap(answerEvaluation, answer.getRace1());
-        fillEvaluationMap(answerEvaluation, answer.getRace2());
-        fillEvaluationMap(answerEvaluation, answer.getRace3());
-        fillEvaluationMap(answerEvaluation, answer.getRace4());
-        fillEvaluationMap(answerEvaluation, answer.getRace5());
-        fillEvaluationMap(answerEvaluation, answer.getRace6());
-        fillEvaluationMap(answerEvaluation, answer.getRace7());
-        fillEvaluationMap(answerEvaluation, answer.getRace8());
-        return getMaxKey(answerEvaluation);
-    }
-
-    private Race getMaxKey(Map<Race, Integer> answerEvaluation) {
-        int maxValue = 0;
-        Race maxKey = null;
-        for (Map.Entry<Race, Integer> entry : answerEvaluation.entrySet()) {
-            if (maxValue < entry.getValue()) {
-                maxValue = entry.getValue();
-                maxKey = entry.getKey();
-            }
-        }
-        return maxKey;
-    }
-
-    private void fillEvaluationMap(Map<Race, Integer> answerEvaluation, Race race) {
-        Integer value = answerEvaluation.getOrDefault(race, 0);
-        answerEvaluation.put(race, value + 1);
-    }
-
-    public List<CharacterEntity> findAll() {
-        return characterRepository.findAll();
-    }
-
-    public CharacterEntity getResultCharacter(Answer answer) {
-        Inventory charInv = Inventory.builder().build();
-        List<Item> startingItems = List.of(
-                Item.builder().itemName("Sword").typeOfItem(ItemEnum.ATTACK).description("He attack.").inventory(charInv).build(),
-                Item.builder().itemName("Shield").typeOfItem(ItemEnum.SHIELD).description("He protect.").inventory(charInv).build(),
-                Item.builder().itemName("Potion").typeOfItem(ItemEnum.CONSUMABLE).description("He heals.").inventory(charInv).build()
-        );
-        charInv.setItems(startingItems);
-        Race characterRace = getResults(answer);
-        return CharacterEntity.builder()
-                .name(answer.getName())
-                .hp(characterRace.hp)
-                .mp(characterRace.mp)
-                .gold(characterRace.gold)
-                .imgRef(characterRace.img)
-                .description(characterRace.description)
-                .inventory(charInv)
-                .attack(characterRace.attack)
-                .defence(characterRace.defence)
-                .race(characterRace)
-                .answer(answer)
-                .build();
-    }
-
-    public Integer getIdByName(String name) {
-        return characterRepository.getCharacterByName(name).orElseThrow().getId();
-    }
-
-    public boolean moveItemtoPlayer(Integer characterId, Integer inventoryId, Integer itemId) {
-        Item item = inventoryService.getItemEntityById(itemId);
-        Inventory inventory = inventoryService.getInventoryEntityById(inventoryId);
-        CharacterEntity character = characterRepository.getById(characterId);
-
-        if (item.getValue() > character.getGold()) {
-            return false;
-        }
-        inventory.getItems().remove(item);
-        character.getInventory().getItems().add(item);
-        item.setInventory(character.getInventory());
-        character.setGold(character.getGold() - item.getValue());
-        return true;
-    }
+    inventory.getItems().remove(item);
+    character.getInventory().getItems().add(item);
+    item.setInventory(character.getInventory());
+    character.setGold(character.getGold() - item.getValue());
+    return true;
+  }
 }
