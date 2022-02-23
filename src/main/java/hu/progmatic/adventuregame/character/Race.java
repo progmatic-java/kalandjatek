dd07ad858d543fdb6051b36f49fc0dce97eb0450
Krisz <krisztian.kovats94@gmail.com>
2022-02-23 11:57:46 +0100

    munka

--- src/main/java/hu/progmatic/adventuregame/character/Race.java
+++ src/main/java/hu/progmatic/adventuregame/character/Race.java
@@ -11,52 +11,52 @@ public enum Race {
             "You are an Orc! You are a fierce fighter who is ready for any challenge!",
             200, 50, 200, 15, 10,
             List.of(
-                    Item.builder().itemName("Mace").typeOfItem(ItemEnum.ATTACK).description("So big.").attack(2).damage(10).build(),
-                    Item.builder().itemName("Rock shield").typeOfItem(ItemEnum.SHIELD).description("So rock.").defence(2).build()
+                    Item.builder().itemName("Mace").typeOfItem(ItemEnum.ATTACK).description("So big.(+2 ATK, 10 DMG)").attack(2).damage(10).build(),
+                    Item.builder().itemName("Rock shield").typeOfItem(ItemEnum.SHIELD).description("So rock.(+2 DEF)").defence(2).build()
             ),
             List.of(
-                    Item.builder().itemName("Battle Axe").typeOfItem(ItemEnum.ATTACK).description("So sharp.").attack(3).damage(10).build(),
-                    Item.builder().itemName("Spoon").typeOfItem(ItemEnum.ATTACK).description("So handy!").attack(1).damage(1).build(),
-                    Item.builder().itemName("Raw meat").typeOfItem(ItemEnum.CONSUMABLE).description("It's a bit chewy").hp(10).build()
+                    Item.builder().itemName("Battle Axe").typeOfItem(ItemEnum.ATTACK).description("So sharp.(+3 ATK, 10 DMG)").attack(3).damage(10).build(),
+                    Item.builder().itemName("Spoon").typeOfItem(ItemEnum.ATTACK).description("So handy!(+1 ATK, 1 DMG)").attack(1).damage(1).build(),
+                    Item.builder().itemName("Raw meat").typeOfItem(ItemEnum.CONSUMABLE).description("It's a bit chewy.(+10 HP)").hp(10).build()
             )
             , "https://i.imgur.com/jZpzqld.png"),
     HUMAN("https://i.imgur.com/MQRZudq.png",
             "You are a Human! You remind me of a piece of stale bread... kinda boring, but it's fine, I guess!",
             150, 100, 200, 10, 16,
             List.of(
-                    Item.builder().itemName("Long Sword").typeOfItem(ItemEnum.ATTACK).description("So long... And a bit rusty").attack(2).damage(8).build(),
-                    Item.builder().itemName("Steel shield").typeOfItem(ItemEnum.SHIELD).description("So hard..").defence(3).build()
+                    Item.builder().itemName("Long Sword").typeOfItem(ItemEnum.ATTACK).description("So long... And a bit rusty.(+2 ATK, 8 DMG)").attack(2).damage(8).build(),
+                    Item.builder().itemName("Steel shield").typeOfItem(ItemEnum.SHIELD).description("So hard..(+3 DEF)").defence(3).build()
             ),
             List.of(
-                    Item.builder().itemName("Fine wine").typeOfItem(ItemEnum.CONSUMABLE).description("So refreshing!").hp(10).build(),
-                    Item.builder().itemName("Liver pâté").typeOfItem(ItemEnum.CONSUMABLE).description("So ... Disgusting!!!!").hp(1).build(),
-                    Item.builder().itemName("Scroll of Lightning").typeOfItem(ItemEnum.CONSUMABLE).description("Thunder and Lightning!").mp(20).damage(20).build()
+                    Item.builder().itemName("Fine wine").typeOfItem(ItemEnum.CONSUMABLE).description("So refreshing!(+10 HP)").hp(10).build(),
+                    Item.builder().itemName("Liver pâté").typeOfItem(ItemEnum.CONSUMABLE).description("So ... Disgusting!!!!(+10 HP)").hp(10).build(),
+                    Item.builder().itemName("Scroll of Lightning").typeOfItem(ItemEnum.CONSUMABLE).description("Thunder and Lightning!(20 DMG, -10 MP)").mp(20).damage(20).build()
             )
             , "https://i.imgur.com/GeilnJE.png"),
     ELF("https://i.imgur.com/13NVorA.png",
             "You are an Elf! You are a magical and mysterious being of human stature who are friendly with nature and animals... also you're pretty hot.",
             100, 200, 200, 8, 20,
             List.of(
-                    Item.builder().itemName("Bow and Arrow").typeOfItem(ItemEnum.ATTACK).description("Fast and accurate.").attack(3).damage(7).build(),
-                    Item.builder().itemName("Mithril chain mail").typeOfItem(ItemEnum.SHIELD).description("So shiny and stylish.").defence(5).build()
+                    Item.builder().itemName("Bow and Arrow").typeOfItem(ItemEnum.ATTACK).description("Fast and accurate.(+3 ATK, 7 DMG)").attack(3).damage(7).build(),
+                    Item.builder().itemName("Mithril chain mail").typeOfItem(ItemEnum.SHIELD).description("So shiny and stylish.(+5 DEF)").defence(5).build()
             ),
             List.of(
                     Item.builder().itemName("Nectar").typeOfItem(ItemEnum.CONSUMABLE).description("Juicy...!").hp(20).build(),
-                    Item.builder().itemName("Elixir of Immortality").typeOfItem(ItemEnum.CONSUMABLE).description("Strengthens your vitality!").mp(30).damage(30).build(),
-                    Item.builder().itemName("Scroll of Thousand Calamities").typeOfItem(ItemEnum.CONSUMABLE).description("Unleashes chaotic forces!").mp(40).damage(40).build()
+                    Item.builder().itemName("Elixir of Immortality").typeOfItem(ItemEnum.CONSUMABLE).description("Strengthens your vitality!(30 DMG, -30 MP)").mp(30).damage(30).build(),
+                    Item.builder().itemName("Scroll of Thousand Calamities").typeOfItem(ItemEnum.CONSUMABLE).description("Unleashes chaotic forces!(40 DMG, -40 MP)").mp(40).damage(40).build()
             )
             , "https://i.imgur.com/bB49TdZ.png"),
     REPTILIAN("https://i.imgur.com/0f45Fce.png",
             "You are a Reptilian Humanoid! You are a brave, yet ssssly fighter! If you were a Hogwarts House, you would be Slytherin.",
             150, 150, 200, 12, 13,
             List.of(
-                    Item.builder().itemName("Trident").typeOfItem(ItemEnum.ATTACK).description("So spiky.").attack(4).damage(8).build(),
-                    Item.builder().itemName("Chitin shield").typeOfItem(ItemEnum.SHIELD).description("So stinky.").defence(3).build()
+                    Item.builder().itemName("Trident").typeOfItem(ItemEnum.ATTACK).description("So spiky.(+4 ATK, 8 DMG)").attack(4).damage(8).build(),
+                    Item.builder().itemName("Chitin shield").typeOfItem(ItemEnum.SHIELD).description("So stinky.(+3 DEF)").defence(3).build()
             ),
             List.of(
-                    Item.builder().itemName("Can of Flies").typeOfItem(ItemEnum.CONSUMABLE).description("So crunchy.").hp(20).build(),
-                    Item.builder().itemName("Acid Spit").typeOfItem(ItemEnum.CONSUMABLE).description("So dangerous.").mp(20).damage(20).build(),
-                    Item.builder().itemName("Hardened Skin").typeOfItem(ItemEnum.SHIELD).description("I wonder whose this was?").defence(3).build()
+                    Item.builder().itemName("Can of Flies").typeOfItem(ItemEnum.CONSUMABLE).description("So crunchy.(+20 HP)").hp(20).build(),
+                    Item.builder().itemName("Acid Spit").typeOfItem(ItemEnum.CONSUMABLE).description("So dangerous.(20 DMG, -20 MP)").mp(20).damage(20).build(),
+                    Item.builder().itemName("Hardened Skin").typeOfItem(ItemEnum.SHIELD).description("I wonder whose this was?(+3 DEF)").defence(3).build()
             )
             , "https://i.imgur.com/dDierSp.png");
 
